e429a587b8e5a260313945f14905a37c2fd9ac38
Praful Meshram <Praful.Meshram@synechron.com>
2019-09-18 16:37:20 +0530

    path variable code added

--- spring-web1/src/main/java/com/demo/controller/StudentController.java
+++ spring-web1/src/main/java/com/demo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.demo.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,8 +24,7 @@ public class StudentController {
 		return "studentRegisterForm";
 	}
 
-	
-	// Access query parameter using HttpServletRequest 
+	// Access query parameter using HttpServletRequest
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String name2(HttpServletRequest request) {
 		System.out.println("register .....");
@@ -48,6 +48,14 @@ public class StudentController {
 	public String name5(@RequestParam String name, @RequestParam("address") String addr, String branch) {
 		System.out.println("register2 .....");
 		System.out.println("name : " + name + "\nbranch : " + branch + "\nadress : " + addr);
+		return "studentRegisterForm";
+	}
+
+	// Access query parameter using @RequestParam annotation
+	@RequestMapping(value = "/register3/{name}/{address}", method = RequestMethod.GET)
+	public String name6(@PathVariable("address") String addr, @PathVariable String name) {
+		System.out.println("register3 .....");
+		System.out.println("name : " + name + "\nadress  : " + addr);
 		return "studentRegisterForm";
 	}
 
